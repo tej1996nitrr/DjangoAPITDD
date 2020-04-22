3cfe201afebe21e806291aa01d7a0b1bf4306c22
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-04-22 14:10:52 +0530

    added hypothesis and slug field

--- school/classroom/models.py
+++ school/classroom/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.exceptions import ValidationError
+
+
+def validate_negative(value):
+    if value<0:
+        raise  ValidationError('Not a positive number')
 
 
 class Student(models.Model):
@@ -6,20 +13,25 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     admission_number = models.IntegerField(unique=True)
     is_qualified = models.BooleanField(default=False)
-    average_score = models.FloatField(blank=True, null=True)
+    average_score = models.FloatField(blank=True, null=True,validators=[validate_negative])
+    username = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.first_name
 
     def get_grade(self):
-        if self.average_score < 40.0:
+        if 0 <= self.average_score < 40.0:
             return "Fail"
-        elif 40.0 < self.average_score < 70.0:
+        elif 40.0 <= self.average_score < 70.0:
             return "Pass"
-        elif 70.0 <self.average_score < 100.0:
+        elif 70.0 <= self.average_score < 100.0:
             return "Excellent"
         else:
             return "Error"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.first_name)
+        super(Student, self).save(*args, **kwargs)
 
 
 class ClassRoom(models.Model):
--- school/classroom/tests.py
+++ school/classroom/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from .models import Student, ClassRoom
 from mixer.backend.django import mixer
-import  pytest
+import pytest
+from hypothesis import given, strategies as st
+from hypothesis.extra.django import TestCase
 # Create your tests here.
 
-pytestmark = pytest.mark.django_db #not to save data to database
+pytestmark = pytest.mark.django_db  # not to save data to database
+
 
 class TestStudentModel(TestCase):
 
@@ -14,35 +17,40 @@ class TestStudentModel(TestCase):
     def test_student_creation(self):
         student = mixer.blend(Student, first_name="Tom")
         student_query = Student.objects.last()
-        self.assertEqual(student_query.first_name, 'Tom')
+        assert student_query.first_name == 'Tom'
 
     def test_string_return(self):
         student = mixer.blend(Student, first_name="Tom")
-        self.assertEqual(str(student), 'Tom')
+        assert str(student) == 'Tom'
 
-    def test_grade_fail(self):
-        student = mixer.blend(Student, average_score=10)
+    @given(st.floats(min_value=0, max_value=40))
+    def test_grade_fail(self, fail_score):
+        student = mixer.blend(Student, average_score=fail_score)
         student_query = Student.objects.last()
-        self.assertEqual(student_query.get_grade(), 'Fail')
+        assert student_query.get_grade() == 'Fail'
 
-    def test_grade_pass(self):
-        student = mixer.blend(Student, average_score=50)
+    @given(st.floats(min_value=40, max_value=70))
+    def test_grade_pass(self, pass_score):
+        student = mixer.blend(Student, average_score=pass_score)
         student_query = Student.objects.last()
-        self.assertEqual(student_query.get_grade(), 'Pass')
+        assert student_query.get_grade() == 'Pass'
 
-    def test_grade_excellent(self):
-        student = mixer.blend(Student, average_score=80)
+    @given(st.floats(min_value=70, max_value=100))
+    def test_grade_excellent(self,score):
+        student = mixer.blend(Student, average_score=score)
         student_query = Student.objects.last()
-        self.assertEqual(student_query.get_grade(), 'Excellent')
+        assert student_query.get_grade() == 'Excellent'
 
-    def test_grade_error(self):
-        student = mixer.blend(Student, average_score=101)
+    @given(st.floats(min_value=100))
+    def test_grade_error(self, error_grade):
+        student = mixer.blend(Student, average_score=error_grade)
         student_query = Student.objects.last()
-        self.assertEqual(student_query.get_grade(), 'Error')
+        assert student_query.get_grade() == 'Error'
+
 
 class TestClassroomModel:
     def test_classroom_create(self):
-        classroom = mixer.blend(ClassRoom,name='Physics')
+        classroom = mixer.blend(ClassRoom, name='Physics')
         classroom_query = ClassRoom.objects.last()
         assert classroom_query.name == "Physics"
         assert str(classroom) == "Physics"
